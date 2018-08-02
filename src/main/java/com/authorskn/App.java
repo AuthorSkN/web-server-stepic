00c01e9916662c1de21d1985cdcf23544f3fd1d5
AuthorSkN <author.skn@gmail.com>
2018-08-02 16:48:16 +0400

    Working first-web-server! Testing

--- src/main/java/com/authorskn/App.java
+++ src/main/java/com/authorskn/App.java
@@ -1,6 +1,7 @@
 package com.authorskn;
 
 import com.authorskn.servlets.AllRequestServletsServlet;
+import com.authorskn.servlets.TestMirrorServlet;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
@@ -9,17 +10,20 @@ import javax.servlet.ServletContext;
 
 public class App {
 
-    private static int PORT = 8080;
+    private static int PORT = 8088;
 
     public static void main(String[] args) throws Exception {
         AllRequestServletsServlet allRequestServletsServlet = new AllRequestServletsServlet();
+        TestMirrorServlet testMirrorServlet = new TestMirrorServlet();
 
         Server server = new Server(PORT);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.addServlet(new ServletHolder(allRequestServletsServlet), "/*");
+        context.addServlet(new ServletHolder(testMirrorServlet), "/mirror");
         server.setHandler(context);
 
         server.start();
+        java.util.logging.Logger.getGlobal().info("Server started");
         server.join();
     }
 
--- src/main/java/com/authorskn/servlets/TestMirrorServlet.java
+++ src/main/java/com/authorskn/servlets/TestMirrorServlet.java
@@ -1,15 +1,23 @@
 package com.authorskn.servlets;
 
+import com.authorskn.templater.PageGenerator;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TestMirrorServlet extends HttpServlet {
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    private static final String KEY = "key";
 
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, Object> data = new HashMap<>();
+        data.put("value", request.getParameter(KEY));
+        response.getWriter().println(PageGenerator.instance().getPage("page_mirror.html", data));
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
